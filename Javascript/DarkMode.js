92aafeec4837e56e8f2128fca55c5ef72676aaf3
Gabriel-sousa-amorim <essecara1310@gmail.com>
2023-06-06 15:01:29 -0300

    Correções e Estilização

--- Javascript/DarkMode.js
+++ Javascript/DarkMode.js
@@ -1,14 +1,20 @@
-var Darkcheckbox = document.getElementById("DarkButton");
-Darkcheckbox.addEventListener("change", function() {
+
+const Darkcheckbox = document.getElementById("DarkButton");
+let i = 0
+function DarkcheckboxToogle() {
     const root = document.querySelector(":root");
     let label = document.querySelector("label.darkModeElement");
+    console.log(i)
     if (this.checked == true) {
         root.style.setProperty('--color-white', "#181818");
         root.style.setProperty('--color-black', "#efefef ");
         label.textContent = "Modo Claro"
+        return i = 1;
     } else {
         root.style.setProperty('--color-white', "#efefef ");
         root.style.setProperty('--color-black', "#181818");
         label.textContent = "Modo Escuro"
+        return i = 0;
     }
-});
+};
+Darkcheckbox.addEventListener("change", DarkcheckboxToogle, true)
