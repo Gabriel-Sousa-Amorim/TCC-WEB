da7362e2c536497e7457d3790c26acb00ac95694
Gabriel-sousa-amorim <essecara1310@gmail.com>
2023-06-14 16:39:29 -0300

    Contatos

--- Javascript/Transition.js
+++ Javascript/Transition.js
@@ -1,5 +1,5 @@
-document.body.style.zoom = "100%"
-function Animation() {
+
+function LockScroll() {
     window.scrollTo(1, 1)
     document.getElementById("Moving-page").animate(
         [
@@ -14,16 +14,56 @@ function Animation() {
         delay: 900,
         fill:"forwards",
         easing: "ease-in"
-    }
-    );
-    
-    const Xposition = window.pageXOffset || document.documentElement.scrollLeft;
-    const Yposition = window.pageYOffset || document.documentElement.scrollTop;
+    });
+    const Xposition = document.documentElement.scrollLeft;
+    const Yposition = document.documentElement.scrollTop;
     window.onscroll = function() {
         window.scrollTo(Xposition, Yposition);
-}
-}
-setTimeout(
-    function() {window.onscroll = {}},
-    1800)
-Animation()
+}}
+
+setTimeout(function() {window.onscroll = {}},1800)
+LockScroll()
+
+console.log(document.querySelectorAll("div.contact-child"))
+//
+//function RotateYCard() {
+//    document.querySelectorAll("div.contact-child").forEach(i =>
+//    {
+//        i.addEventListener("mouseenter", function() {
+//
+//            i.animate(
+//                [
+//                    {transform: "rotateY(0deg) rotateX(0deg)" },
+//                    {transform: "rotateY(180deg) rotateX(180deg)" },
+//                ], 
+//            {
+//                // timing options
+//                duration: 500,
+//                iterations: 1,
+//                delay: 0,
+//                easing: "ease-in",
+//                direction:"alternate",
+//                fill: "forwards"
+//            });
+//        })
+//        i.addEventListener("mouseout", function() {
+//
+//            i.animate(
+//                [
+//                    {transform: "rotateY(180deg) rotateX(180deg)" },
+//                    {transform: "rotateY(0deg) rotateX(0deg)" },
+//                ], 
+//            {
+//                // timing options
+//                duration: 500,
+//                iterations: 1,
+//                delay: 0,
+//                easing: "ease-in",
+//                direction:"alternate",
+//                fill: "forwards"
+//            });
+//        })
+//    }
+//    )
+//}
+//RotateYCard()
