f27b7e1047ced99c639d560b2e72361cfd147bc8
Gabriel-Sousa-Amorim <essecara1310@gmail.com>
2023-06-14 21:59:17 -0300

    Novas Features

--- Javascript/DarkMode.js
+++ Javascript/DarkMode.js
@@ -4,15 +4,23 @@ let i = 0
 function DarkcheckboxToogle() {
     const root = document.querySelector(":root");
     let label = document.querySelector("label.darkModeElement");
-    console.log(i)
     if (this.checked == true) {
         root.style.setProperty('--color-white', "#202020");
         root.style.setProperty('--color-black', "#efefef ");
         label.textContent = "Modo Claro"
+        console.log(i)
+        return i = 1;
     } else {
         root.style.setProperty('--color-white', "#efefef ");
         root.style.setProperty('--color-black', "#202020");
+        console.log(i)
         label.textContent = "Modo Escuro"
+        return i = 0;
     }
 };
 Darkcheckbox.addEventListener("change", DarkcheckboxToogle, true)
+
+function ThemeReconizer() {
+    localStorage.setItem("theme", i)
+}
+ThemeReconizer()
