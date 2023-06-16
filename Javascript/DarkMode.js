41622ad49a61b2b35c42620606339c7ad69d9470
Gabriel-sousa-amorim <essecara1310@gmail.com>
2023-06-16 16:59:43 -0300

    Icones

--- Javascript/DarkMode.js
+++ Javascript/DarkMode.js
@@ -1,18 +1,16 @@
-
-const Darkcheckbox = document.getElementById("DarkButton");
 function DarkcheckboxToogle() {
     const root = document.querySelector(":root");
     let label = document.querySelector("label.darkModeElement");
     if (this.checked == true) {
-        root.style.setProperty('--color-white', "#202020");
-        root.style.setProperty('--color-black', "#efefef ");
-        label.classList.replace("bi-brightness-high", "bi-moon-stars-fill")
-        label.textContent = "Modo Escuro"
-    } else {
         root.style.setProperty('--color-white', "#efefef ");
         root.style.setProperty('--color-black', "#202020");
         label.classList.replace("bi-moon-stars-fill", "bi-brightness-high" )
         label.textContent = "Modo Claro"
+    } else {
+        root.style.setProperty('--color-white', "#202020");
+        root.style.setProperty('--color-black', "#efefef ");
+        label.classList.replace("bi-brightness-high", "bi-moon-stars-fill")
+        label.textContent = "Modo Escuro"
     }
 };
-Darkcheckbox.addEventListener("change", DarkcheckboxToogle, true)
+document.getElementById("DarkButton").addEventListener("change", DarkcheckboxToogle, true)
